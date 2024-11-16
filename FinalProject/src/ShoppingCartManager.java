d0b8cb82ca3bfafba437394010de664ffbc80bb9
Ayush-des <Ayushdesai0622@gmail.com>
2024-11-15 19:11:16 -0500

    cook

--- FinalProject/src/ShoppingCartManager.java
+++ FinalProject/src/ShoppingCartManager.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class ShoppingCartManager {
 
 	public static void main(String[] args) {
@@ -7,23 +8,7 @@ public class ShoppingCartManager {
 		Scanner input = new Scanner(System.in);
 		
 		
-		Shop store = new Shop();
-		
-
-		for(int i = 0; i<2;  i++)
-		{
-			ItemToPurchase item= new ItemToPurchase("ToothBrush (10 pack)", "Self Care", i+4, i);
-			store.addItemToShop(item);
-		}
-		
-		for(int i = 0; i<2;  i++)
-		{
-			Produce item= new Produce("Milk", "Dairy", i+4, i, "10/15/2024");
-			store.addItemToShop(item);
-		}
-		
-		
-		System.out.println("Enter  name:");
+		System.out.println("Enter customer's name:");
 		String name = input.nextLine();
 		
 		System.out.println("Enter today's date:");
@@ -39,7 +24,6 @@ public class ShoppingCartManager {
 		ShoppingCart cart = new ShoppingCart(name, date);
 		
 		
-		
 		boolean done = false;
 		while (!done)
 		{
@@ -48,9 +32,12 @@ public class ShoppingCartManager {
 			
 			int goodOptions [] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 			int option = input.nextInt();
-			input.nextLine();
-					
+			
+			
 			boolean correctInput = false;
+			
+			
+			
 			
 			for(int i=0; i<goodOptions.length; i++)
 			{
@@ -75,12 +62,10 @@ public class ShoppingCartManager {
 			}
 			
 			
-			if (option == 9) 
-			{ 
-				done = true; 
-			}
+
 			
-			executeMenu(option, cart, input, store);	
+			executeMenu(option, cart, input);	
+			
 			
 			if (!done)
 				{
@@ -97,61 +82,41 @@ public class ShoppingCartManager {
 		System.out.println("Choose one of the following options:");
 		System.out.println("1. View full catalogue");
 		System.out.println("2. Filter");
-		System.out.println("3. Change item quantity"); //IDK about this one 
+		System.out.println("3. Output item descriptions");
 		System.out.println("4. View cart");
 		System.out.println("5. Add to cart");
 		System.out.println("6. Remove from cart");
 		System.out.println("7. Purchase items in cart"); 
-		System.out.println();
+		System.out.println("8. Apply Promo Code"); 
 		System.out.println("9. Exit Shop");
 		System.out.println();
 	}
 	
-	public static void executeMenu(int number, ShoppingCart cart, Scanner input, Shop store)
+	public static void executeMenu(int number, ShoppingCart cart, Scanner input)
 	{
 		switch (number)
 		{
 		case 1:
 		   System.out.println("Full catalogue of items: ");
 		    //call to method to output all items
-		   
-		   Shop.printRealShop();
-
+		    
 		    break;
 
 		case 2:
 			System.out.println("Choose filter option: ");
-			System.out.println("1. Price (highest to lowest)");
-			System.out.println("2. Perishables in stock");
-			System.out.println("3. Only in stock items");
+			System.out.println("1. Price");
+			System.out.println("2. Perishables");
 			
 			int filter = input.nextInt();
-			
-			if(filter == 1)
-			{
-				Shop.filterPrice();
-			}else if(filter == 2)
-			{
-				Shop.printProduce();
-			}else if(filter ==3 )
-			{
-				Shop.printInStockItems();
-			}else {
-				System.out.println("Invalid input");
-			}
-			
+			//price logic
+			//perishables logic 
+			/*
+			 * use if statement for this?
+			 */
 			break;
 		case 3:
-			System.out.println("CHANGE ITEM QUANITY IN CART");
-
-			System.out.println("Enter the item name:");
-		    String changeItemName = input.nextLine();
-		    System.out.println("Enter the item quantity:");
-		    int newQuantity = input.nextInt();
-		    input.nextLine();
-		    
-			cart.changeQuantity(changeItemName, newQuantity);
-			
+			System.out.println("OUTPUT ITEMS' DESCRIPTIONS");
+			cart.printDescriptions();
 			
 			break;
 		case 4:
@@ -163,10 +128,6 @@ public class ShoppingCartManager {
 			System.out.println("ADD ITEM TO CART");
 		    System.out.println("Enter the item name:");
 		    String name = input.nextLine();
-		    System.out.println("Enter the item quantity:");
-		    int quantity = input.nextInt();
-		    input.nextLine();
-		    cart.addItem(name,quantity,store);
 		  
 			break;
 		case 6:
@@ -179,38 +140,30 @@ public class ShoppingCartManager {
 			
 		case 7: //purchasing items in cart
 			System.out.println("PURCHASE ITEMS IN CART");
-			
-			cart.printTotal();
-			
-			if(cart.getNumItemsInCart() > 0)
+			System.out.println("Would you like to purchase these items? (y/n)");
+			String option = input.next();
+			if(option.equalsIgnoreCase("y"))
 			{
-				System.out.println("Would you like to purchase these items? (yes/no)");
-				String option = input.nextLine();
-				if(option.equalsIgnoreCase("yes"))
-				{
-					//call checkout method to check out items
-					Checkout purhase = new Checkout();
-					purhase.checkout(cart,input);
-				}
-				else if(option.equalsIgnoreCase("no"))
-				{
-					System.out.println("Checkout cancelled.");
-				}
-				else
-				{
-					System.out.println("Please enter a valid input. ");
-				}
-			}else {
-				System.out.println("There are no items in the cart. Please add items to checkout.");
+				//call checkout method to check out items
 			}
+			else if(option.equalsIgnoreCase("n"))
+			{
+				System.out.println("Your purchase has been cancelled.");
+			}
+			else
+			{
+				System.out.println("Please enter a valid input. ");
+			}
+			break;
 			
+		case 8: //Apply a promo code
 			break;
 		case 9: //exit the store
 			System.out.println("You are now exiting the store. Thank you for shopping with us!");
 			break;
+		default: 
+			System.out.println("Choose an option");
 		}
-	}
-	
-}
+	}}
 	
 
